a88fd1c3f8094f7e7690e654bcceaacceb885bcf
hahwul <hahwul@gmail.com>
2020-10-31 01:07:35 +0900

    Brew formula update for jwt-hack version v1.0.3

--- Formula/jwt-hack.rb
+++ Formula/jwt-hack.rb
@@ -2,16 +2,16 @@
 class JwtHack < Formula
   desc "Hack the JWT(JSON Web Token) / jwt-hack is JWT hacking, security testing utility"
   homepage "https://www.hahwul.com"
-  version "1.0.2"
+  version "1.0.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/hahwul/jwt-hack/releases/download/v1.0.2/jwt-hack_1.0.2_darwin_amd64.tar.gz"
-    sha256 "d2f01282ef2fdf9a60dd80a5fe71235e2b03f51589ba2aa21a453f7540fa286f"
+    url "https://github.com/hahwul/jwt-hack/releases/download/v1.0.3/jwt-hack_1.0.3_darwin_amd64.tar.gz"
+    sha256 "ad644c7990e52f5645c17487d886f9a43c9e03d0672110f9effb2dab92eb15e1"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/hahwul/jwt-hack/releases/download/v1.0.2/jwt-hack_1.0.2_linux_amd64.tar.gz"
-      sha256 "e4a0d39e2bdf8d2c77af9600ec1c2390d10b1b8b0d7cdca4328e6730ff215e6a"
+      url "https://github.com/hahwul/jwt-hack/releases/download/v1.0.3/jwt-hack_1.0.3_linux_amd64.tar.gz"
+      sha256 "0d7c6ace576b08d31daa3a272b44179230d6d1a6e763c89d15d67f42d12a63d3"
     end
   end
 
